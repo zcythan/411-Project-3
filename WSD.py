ff24f893e6960134832b96ab6ef52c3978633249
zcythan <jferrick@umich.edu>
2023-10-25 23:14:55 -0400

    basic folds being constructed

--- WSD.py
+++ WSD.py
@@ -1,5 +1,65 @@
+import sys
+
+class WSD:
+    def __init__(self, file):
+        self.__file = file
+        #Has 5 keys for each fold, each value is a tuple containing word count in index 0 and
+        # a dictionary containing a key of the sense and a value of the count in index 1.
+        self.folds = self.__buildFolds()
+
+    def __buildFolds(self):
+        with open(self.__file, 'r') as file:
+            data = file.readlines()
+            size = len(data)/5
+            folds = {}
+            senseDict = {}
+            wCount = 0
+            fCount = 1
+            fid = 1
+            for line in data:
+                if fCount > size:
+                    folds[fid] = (wCount, senseDict)
+                    fCount = 1
+                    fid += 1
+                    wCount = 0
+                    senseDict = {}
+
+                if "senseid=" in line:
+                    curSense = line[line.rfind("senseid=")+9:line.rfind('/')-1]
+                    if curSense in senseDict:
+                        senseDict[curSense] += 1
+                    else:
+                        senseDict[curSense] = 1
+                if "<head>" in line:
+                    wCount += 1
+
+                fCount += 1
+
+                #Use if multiple words ever occur/we need a reference to the word in a training set.
+                '''
+                if "<head>" in line:
+                    curHead = line[line.rfind("<head>")+6:line.rfind("</head>")]
+                    if curHead in countDict:
+                        countDict[curHead] += 1
+                    else:
+                        countDict[curHead] = 1
+                '''
+        return folds
+
+
 def main():
-    print("Hello, World.")  # Press Ctrl+F8 to toggle the breakpoint.
+    if len(sys.argv) < 2:
+        print("Too few input arguments")
+        return
+    AI = WSD(sys.argv[1])
+
+    for key, value in AI.folds.items():
+        print("New Fold: ")
+        count = value[0]
+        for ke, val in value[1].items():
+            print ("Key: " + ke + " val: " + str(val))
+        print(count)
+
 
 if __name__ == '__main__':
     main()
